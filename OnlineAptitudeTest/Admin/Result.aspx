f86aa36b8a3489745274e31ff226467c94f64953
Shahzaib <shahzaibux@gmail.com>
2020-05-05 02:15:04 +0500

    Created Result in Admin

---
+++ OnlineAptitudeTest/Admin/Result.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="Result.aspx.cs" Inherits="OnlineAptitudeTest.Admin.Result" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+    <div class="col-md-12">
+        <div class="card">
+            <%--Button For select panel--%>
+            <div class="btn-group bg-warning">
+                <asp:Button ID="btn_PanelResult" runat="server" Text="All result" CssClass="btn btn-info" BorderStyle="None" CausesValidation="False" BackColor="#000222" />
+            </div>
+            <div class="card text-center mb-3">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <asp:GridView ID="gridViewSpecific" runat="server" GridLines="None" AllowPaging="True" AutoGenerateColumns="False" CssClass="table table-bordered" PageSize="8" Visible="false" OnPageIndexChanging="gridViewSpecific_PageIndexChanging">
+                            <Columns>
+                                <asp:BoundField DataField="user_email" HeaderText="User email" />
+                                <asp:BoundField DataField="interview_name" HeaderText="Interview Name" NullDisplayText="no interview name" />
+                                <asp:BoundField DataField="interview_date" DataFormatString="{0:M/d/yy}" HeaderText="Interview Date" NullDisplayText="No date" />
+                                <asp:BoundField DataField="totalquestion" HeaderText="Total Question" />
+                                <asp:BoundField DataField="result_status" HeaderText="Result" />
+                                <asp:BoundField DataField="interview_marks" HeaderText="Total Marks" />
+                                <asp:BoundField DataField="result_score" HeaderText="Your Score" />
+                            </Columns>
+                        </asp:GridView>
+                        <asp:GridView ID="gridResult" runat="server" GridLines="None" AllowPaging="True" AutoGenerateColumns="False" CssClass="table table-bordered" PageSize="8" OnPageIndexChanging="gridResult_PageIndexChanging">
+                            <Columns>
+                                <asp:BoundField DataField="user_email" HeaderText="User email" />
+                                <asp:BoundField DataField="interview_name" HeaderText="Interview Name" NullDisplayText="no interview name" />
+                                <asp:BoundField DataField="interview_date" DataFormatString="{0:M/d/yy}" HeaderText="Interview Date" NullDisplayText="No date" />
+                                <asp:BoundField DataField="totalquestion" HeaderText="Total Question" />
+                                <asp:BoundField DataField="result_status" HeaderText="Result" />
+                                <asp:BoundField DataField="interview_marks" HeaderText="Total Marks" />
+                                <asp:BoundField DataField="result_score" HeaderText="Your Score" />
+                            </Columns>
+                        </asp:GridView>
+                    </div>
+                </div>
+                <asp:Panel ID="panel_ResultShow_Warning" runat="server" Visible="false">
+                    <div class="card-footer">
+                        <br />
+                        <div class="alert alert-danger text-center">
+                            <asp:Label ID="lbl_ResultShowWarning" runat="server" />
+                        </div>
+                    </div>
+                </asp:Panel>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Admin/Result.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest.Admin
+{
+    public partial class Result : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string candidateemail = Request.QueryString["uid"];
+            if (!IsPostBack)
+            {
+                if (candidateemail != null)
+                {
+                    getSpecificResults(candidateemail);
+                    gridViewSpecific.Visible = true;
+                    gridResult.Visible = false;
+                }
+                else
+                {
+                    getAllResults();
+                    gridViewSpecific.Visible = false;
+                    gridResult.Visible = true;
+                }
+
+            }
+        }
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+        //method for get all result
+        public void getAllResults()
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Result left join Interview on result.interview_fid = interview.interview_id", con);
+                try
+                {
+                    con.Open();
+                    using (SqlDataAdapter ad = new SqlDataAdapter())
+                    {
+                        ad.SelectCommand = cmd;
+                        using (DataTable tb = new DataTable())
+                        {
+                            ad.Fill(tb);
+                            if (tb != null)
+                            {
+                                gridResult.DataSource = tb;
+                                gridResult.DataBind();
+                            }
+                            else
+                            {
+                                panel_ResultShow_Warning.Visible = true;
+                                lbl_ResultShowWarning.Text = "There is no result right now in this application";
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    panel_ResultShow_Warning.Visible = true;
+                    lbl_ResultShowWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+            }
+        }
+
+        //method for spexific result
+
+        public void getSpecificResults(string email)
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Result left join Interview on result.interview_fid = interview.interview_id where candidate_email = @uemail", con);
+                cmd.Parameters.AddWithValue("@uemail", email);
+                try
+                {
+                    con.Open();
+                    using (SqlDataAdapter ad = new SqlDataAdapter())
+                    {
+                        ad.SelectCommand = cmd;
+                        using (DataTable tb = new DataTable())
+                        {
+                            ad.Fill(tb);
+                            if (tb != null)
+                            {
+                                gridViewSpecific.DataSource = tb;
+                                gridViewSpecific.DataBind();
+                            }
+                            else
+                            {
+                                panel_ResultShow_Warning.Visible = true;
+                                lbl_ResultShowWarning.Text = "There is no result right now in this application";
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    panel_ResultShow_Warning.Visible = true;
+                    lbl_ResultShowWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+            }
+        }
+        //for paging
+        protected void gridResult_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridResult.PageIndex = e.NewPageIndex;
+            getAllResults();
+            gridViewSpecific.Visible = false;
+            gridResult.Visible = true;
+        }
+
+        // for specific result paging
+        protected void gridViewSpecific_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            string uemail = Request.QueryString["uid"];
+            gridViewSpecific.PageIndex = e.NewPageIndex;
+            getSpecificResults(uemail);
+            gridViewSpecific.Visible = true;
+            gridResult.Visible = false;
+        }
+    }
+}
