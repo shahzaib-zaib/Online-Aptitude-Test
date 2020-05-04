ec81b45e5a623c02f2a27437a07d014f5c599d32
Shahzaib <shahzaibux@gmail.com>
2020-05-05 00:08:39 +0500

    Created Candidate Criteria Page

---
+++ OnlineAptitudeTest/Criteria.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Candidatemaster.Master" AutoEventWireup="true" CodeFile="Criteria.aspx.cs" Inherits="OnlineAptitudeTest.Criteria" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="heardcontentplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontentplaceholder" runat="server">
+    <h2 class="m-4">All
+        <asp:Label ID="lbl_criteriainterview" runat="server"></asp:Label>
+    </h2>
+    <hr />
+    <div class="row">
+        <asp:Repeater ID="gridview_criteriainterview" runat="server">
+            <ItemTemplate>
+                <div class="col-lg-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h4>Test name:</h4>
+                            <h3><%# Eval("interview_name") %></h3>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Interview discription: <%# Eval("interview_description") %></li>
+                            <li class="list-group-item">Pass Marks: <%# Eval("interviewpass_marks") %></li>
+                            <li class="list-group-item">Total Marks: <%# Eval("interview_marks") %></li>
+                            <li class="list-group-item">
+                                <asp:HyperLink ID="hlink_gotexam" runat="server" CssClass="btn btn-primary" ForeColor="White" NavigateUrl='<%# "~/Question.aspx?eid=" + Eval("interview_id")%>'>Take Test</asp:HyperLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </ItemTemplate>
+
+        </asp:Repeater>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <asp:Panel ID="panel_interviewshow_warning" runat="server" Visible="false">
+                        <br />
+                        <div class="alert alert-danger text-center">
+                            <asp:Label ID="lbl_interviewshowwarning" runat="server" />
+                        </div>
+                    </asp:Panel>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Criteria.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest
+{
+    public partial class Criteria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string sid = Request.QueryString["sid"];
+            if (sid == null)
+            {
+                Response.Redirect("Category.aspx");
+            }
+            interviewlistmethod(Convert.ToInt32(sid));
+            criterianamemethod(Convert.ToInt32(sid));
+        }
+
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+        //method for examlist
+        public void interviewlistmethod(int id)
+        {
+
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("spInterviewSearchCategory", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@criteriaid", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.HasRows)
+                    {
+                        gridview_criteriainterview.DataSource = rd;
+                        gridview_criteriainterview.DataBind();
+                    }
+                    else
+                    {
+                        panel_interviewshow_warning.Visible = true;
+                        lbl_interviewshowwarning.Text = "Sorry! There is no interview in this criteria";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_interviewshow_warning.Visible = true;
+                    lbl_interviewshowwarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem" + ex.Message;
+                }
+            }
+        }
+        //method for sujectlist
+        public void criterianamemethod(int id)
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Criteria where criteria_id = @criteriaid", con);
+                cmd.Parameters.AddWithValue("@criteriaid", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        lbl_criteriainterview.Text = rd["criteria_name"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_interviewshow_warning.Visible = true;
+                    lbl_interviewshowwarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem " + ex.Message;
+                }
+            }
+        }
+    }
+}
