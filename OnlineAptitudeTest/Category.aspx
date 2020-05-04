851a803c60b600dd055c10d6da039cbbadbde8da
Shahzaib <shahzaibux@gmail.com>
2020-05-04 23:53:51 +0500

    Created Candidate Category Page

---
+++ OnlineAptitudeTest/Category.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Candidatemaster.Master" AutoEventWireup="true" CodeFile="Category.aspx.cs" Inherits="OnlineAptitudeTest.Category" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="heardcontentplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontentplaceholder" runat="server">
+    <h2 class="m-4 mb-5">
+            All <asp:Label ID="lbl_categoryscriteria" runat="server"></asp:Label> Aptitude Test</h2>
+        <hr />
+    <div class="row">
+                <asp:Repeater ID="gridview_categoryitem" runat="server">
+            <ItemTemplate>
+                <div class="col-lg-4 mb-3">
+                    <div class="card h-100 text-center">
+                        <h4 class="card-header"><%# Eval("criteria_name") %></h4>
+                        <div class="card-footer">
+                            <asp:HyperLink ID="btn_category" runat="server" CssClass="btn btn-success" ForeColor="White" NavigateUrl='<%# "~/Criteria.aspx?sid=" +  Eval("criteria_id") %>'>Go to Test</asp:HyperLink>
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <asp:Panel ID="panel_scriteriashow_warning" runat="server" Visible="false">
+                        <br />
+                        <div class="alert alert-danger text-center">
+                            <asp:Label ID="lbl_scriteriashowwarning" runat="server" />
+                        </div>
+                    </asp:Panel>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Category.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cid = Request.QueryString["cid"];
+            if (cid == null)
+            {
+                Response.Redirect("Index.aspx");
+            }
+            criterialistmethod(Convert.ToInt32(cid));
+            categorynamemethod(Convert.ToInt32(cid));
+        }
+
+
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+
+        //method for subjectlist
+        public void criterialistmethod(int id)
+        {
+
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Criteria where category_fid = @categoryid", con);
+                cmd.Parameters.AddWithValue("@categoryid", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.HasRows)
+                    {
+                        gridview_categoryitem.DataSource = rd;
+                        gridview_categoryitem.DataBind();
+                    }
+                    else
+                    {
+                        panel_scriteriashow_warning.Visible = true;
+                        lbl_scriteriashowwarning.Text = "Sorry! There is no scriteria in this category";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_scriteriashow_warning.Visible = true;
+                    lbl_scriteriashowwarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem" + ex.Message;
+                }
+            }
+        }
+        //method for categorylist
+        public void categorynamemethod(int id)
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Category where category_id = @categoryid", con);
+                cmd.Parameters.AddWithValue("@categoryid", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        lbl_categoryscriteria.Text = rd["category_name"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_scriteriashow_warning.Visible = true;
+                    lbl_scriteriashowwarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem " + ex.Message;
+                }
+            }
+        }
+    }
+}
