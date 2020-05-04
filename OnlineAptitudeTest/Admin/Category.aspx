f72c9512b9f2de8079579012135cb34a056f7175
Shahzaib <shahzaibux@gmail.com>
2020-05-05 01:28:27 +0500

    Created Category in Admiin

---
+++ OnlineAptitudeTest/Admin/Category.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="Category.aspx.cs" Inherits="OnlineAptitudeTest.Admin.Category" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+    <div class="col-md-12">
+        <div class="card">
+            <%--Button For select panel--%>
+            <div class="btn-group bg-warning">
+                <asp:Button ID="btn_panelCategoryList" runat="server" Text="Category List" CssClass="btn btn-info" OnClick="btn_panelCategoryList_Click" BorderStyle="None" CausesValidation="False" />
+                <asp:Button ID="btn_panelAddCategory" runat="server" Text="Add Category" CssClass="btn btn-info" OnClick="btn_panelAddCategory_Click" BorderStyle="None" CausesValidation="False" />
+            </div>
+            <%--Add category panel--%>
+            <asp:Panel ID="panel_AddCategory" runat="server">
+                <div class="card-body">
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Category Name</label>
+                        <div class="col-md-4">
+                            <asp:TextBox ID="txt_category" runat="server" CssClass="form-control"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="require_category" runat="server" ErrorMessage="Enter category" ControlToValidate="txt_category" ForeColor="red"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="offset-2">
+                            <asp:Button ID="btn_AddCategory" runat="server" Text="Add Category" CssClass="btn" BackColor="#82e0aa" BorderStyle="None" ForeColor="White" OnClick="btn_AddCategory_Click" />
+                        </div>
+                        <asp:Panel ID="panel_AddCategory_Warning" runat="server" Visible="false">
+                            <br />
+                            <div class="alert alert-danger text-center">
+                                <asp:Label ID="lbl_CategoryAddWarning" runat="server" />
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+            </asp:Panel>
+            <%--category list panel--%>
+            <asp:Panel ID="panel_categorylist" runat="server">
+                <div class="card text-center mb-3">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <asp:GridView ID="grdview_CategoryList" runat="server" CssClass="table table-bordered" GridLines="None" AutoGenerateColumns="False" OnRowCommand="grdview_CategoryList_RowCommand" AllowPaging="True" PageSize="5" OnPageIndexChanging="grdview_CategoryList_PageIndexChanging">                              
+                                 <Columns>
+                                    <asp:BoundField DataField="category_name" HeaderText="Category Name" >
+                                     <HeaderStyle CssClass="card-header" />
+                                     </asp:BoundField>
+                                    <asp:TemplateField HeaderText="Options">
+                                        <HeaderStyle CssClass="card-header" />
+                                        <ItemTemplate>
+                                            <asp:HyperLink ID="btn_EditCategoryx" runat="server" CssClass="btn" BackColor=" #3498db" BorderStyle="None" ForeColor="White" NavigateUrl='<%# "~/Admin/EditCategory.aspx?cid=" + Eval("category_id")%>'>
+                                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                                            </asp:HyperLink>
+                                            <asp:LinkButton ID="btn_DeleteCategory" runat="server" CssClass="btn" BackColor=" #e74c3c" BorderStyle="None" ForeColor="White" CommandArgument='<%# Eval("category_id") %>' CommandName="delete_category">
+                                            <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                            </asp:LinkButton>
+                                        </ItemTemplate>
+                                    </asp:TemplateField>
+                                </Columns>
+                                <EmptyDataTemplate>
+                                    There is no category right now. Create now
+                                </EmptyDataTemplate>
+                                 <PagerStyle CssClass="card-footer" HorizontalAlign="Right" />
+                            </asp:GridView>
+                        </div>
+                        <asp:Panel ID="panel_CategoryList_Warning" runat="server" Visible="false">
+                            <div class="card-footer">
+                                <br />
+                                <div class="alert alert-danger text-center">
+                                    <asp:Label ID="lbl_CategoryListWarning" runat="server" />
+                                </div>
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+            </asp:Panel>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Admin/Category.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                panel_categorylist.Visible = true;
+                panel_AddCategory.Visible = false;
+                btn_panelCategoryList.BackColor = ColorTranslator.FromHtml("#000222");
+                btn_panelAddCategory.BackColor = ColorTranslator.FromHtml("#ffc107");
+                categorylistmethod();
+            }
+        }
+        //This is button for the enable list of category panel 
+        protected void btn_panelCategoryList_Click(object sender, EventArgs e)
+        {
+            panel_categorylist.Visible = true;
+            panel_AddCategory.Visible = false;
+            btn_panelCategoryList.BackColor = ColorTranslator.FromHtml("#000222");
+            btn_panelAddCategory.BackColor = ColorTranslator.FromHtml("#ffc107");
+            categorylistmethod();
+
+        }
+        //This is button for enable the adding in category panel
+        protected void btn_panelAddCategory_Click(object sender, EventArgs e)
+        {
+            txt_category.Focus();
+            panel_categorylist.Visible = false;
+            panel_AddCategory.Visible = true;
+            btn_panelCategoryList.BackColor = ColorTranslator.FromHtml("#ffc107");
+            btn_panelAddCategory.BackColor = ColorTranslator.FromHtml("#000222");
+        }
+
+        //This is for adding the category in databse 
+        protected void btn_AddCategory_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+
+                using (SqlConnection con = new SqlConnection(s))
+                {
+                    SqlCommand cmd = new SqlCommand("insert into Category (category_name) values (@category_name)", con);
+                    cmd.Parameters.AddWithValue("@category_name", txt_category.Text);
+                    try
+                    {
+                        con.Open();
+                        int i = (int)cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            txt_category.Text = string.Empty;
+                            Response.Redirect("~/Admin/Category.aspx");
+                            Response.Write("Added Succesfully");
+                        }
+                        else
+                        {
+                            txt_category.Focus();
+                            panel_AddCategory_Warning.Visible = true;
+                            lbl_CategoryAddWarning.Text = "Something went wrong";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        txt_category.Focus();
+                        panel_AddCategory_Warning.Visible = true;
+                        lbl_CategoryAddWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                    }
+
+                }
+            }
+            else
+            {
+                txt_category.Focus();
+                panel_AddCategory_Warning.Visible = true;
+                lbl_CategoryAddWarning.Text = "You must fill all the requirements";
+            }
+
+        }
+        // For row command argument
+        protected void grdview_CategoryList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "delete_category")
+            {
+                deletecategory(Convert.ToInt32(e.CommandArgument));
+                categorylistmethod();
+            }
+        }
+        // from page index changing 
+        protected void grdview_CategoryList_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdview_CategoryList.PageIndex = e.NewPageIndex;
+            categorylistmethod();
+        }
+
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+        //Mehtod for retriving category into list item 
+        public void categorylistmethod()
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Category", con);
+                try
+                {
+                    con.Open();
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            grdview_CategoryList.DataSource = dt;
+                            grdview_CategoryList.DataBind();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_CategoryList_Warning.Visible = true;
+                    lbl_CategoryListWarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem" + ex.Message;
+                }
+            }
+        }
+
+        //Method for deleting category
+        public void deletecategory(int id)
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("delete category  where category_id = @catgeryid", con);
+                cmd.Parameters.AddWithValue("@catgeryid", id);
+                try
+                {
+                    con.Open();
+                    int i = (int)cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        Response.Redirect("~/Admin/Category.aspx");
+                        Response.Write("Delete Succesfully");
+                    }
+                    else
+                    {
+                        panel_CategoryList_Warning.Visible = true;
+                        lbl_CategoryListWarning.Text = "Something went wrong. Can't delete now";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_CategoryList_Warning.Visible = true;
+                    lbl_CategoryListWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+
+            }
+        }
+    }
+}
