c726a7ca8fec0455451ff0c9eb4eb562a5f3d07b
Shahzaib <shahzaibux@gmail.com>
2020-05-05 01:55:10 +0500

    Created EditCriteria in Admin

---
+++ OnlineAptitudeTest/Admin/EditCriteria.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="EditCriteria.aspx.cs" Inherits="OnlineAptitudeTest.Admin.EditCriteria" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+    <div class=" col-md-12">
+        <div class="card">
+         <%--Button For edit subject slect--%>
+            <div class="btn-group bg-warning">
+                <asp:Button ID="btn_panelCriteriaList" runat="server" Text="Edit Subject" CssClass="btn btn-info" BorderStyle="None" CausesValidation="False" BackColor="#000222" />
+            </div>
+            <div class="card-body">
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Select Category</label>
+                        <div class="col-md-4">
+                            <asp:DropDownList ID="drp_categoryedit" runat="server" CssClass="form-control" DataTextField="category_name" DataValueField="category_id">
+                            </asp:DropDownList>
+                            <asp:RequiredFieldValidator ID="require_drpcategory" runat="server" ErrorMessage="You must select a category" ControlToValidate="drp_categoryedit" ForeColor="red" InitialValue="-1"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Criteria Name</label>
+                        <div class="col-md-4">
+                            <asp:TextBox ID="txt_criteriaedit" runat="server" CssClass="form-control"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="require_criteria" runat="server" ErrorMessage="Enter subject" ControlToValidate="txt_criteriaedit" ForeColor="red"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="offset-2">
+                            <asp:Button ID="btn_EditCriteria" runat="server" Text="Edit Criteria" CssClass="btn" BackColor="#82e0aa" BorderStyle="None" ForeColor="White" OnClick="btn_EditCriteria_Click" />
+                        </div>
+                        <asp:Panel ID="panel_EditCriteria_Warning" runat="server" Visible="false">
+                            <br />
+                            <div class="alert alert-danger text-center">
+                                <asp:Label ID="lbl_EditCriteria_Warning" runat="server" />
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Admin/EditCriteria.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest.Admin
+{
+    public partial class EditCriteria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string sid = Request.QueryString["sid"];
+                if (sid == null)
+                {
+                    Response.Redirect("~/Admin/Criteria.aspx");
+                }
+                txt_criteriaedit.Focus();
+                get_categorydrp(); // calling for dropdown method
+                categoryedit_fill(Convert.ToInt32(sid)); //calling for text method
+
+
+            }
+        }
+        protected void btn_EditCriteria_Click(object sender, EventArgs e)
+        {
+            string sid = Request.QueryString["sid"];
+            if (IsValid)
+            {
+                using (SqlConnection con = new SqlConnection(s))
+                {
+                    SqlCommand cmd = new SqlCommand("spCriteriaEdit", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@criteria_name", txt_criteriaedit.Text);
+                    cmd.Parameters.AddWithValue("@criteria_id", Convert.ToInt32(sid));
+                    cmd.Parameters.AddWithValue("@category_id", drp_categoryedit.SelectedValue);
+                    try
+                    {
+                        con.Open();
+                        int i = (int)cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            Response.Redirect("~/Admin/Criteria.aspx");
+                        }
+                        else
+                        {
+                            txt_criteriaedit.Focus();
+                            panel_EditCriteria_Warning.Visible = true;
+                            lbl_EditCriteria_Warning.Text = "Something went wrong. Can't update. Please try after sometime later</br> ";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        txt_criteriaedit.Focus();
+                        panel_EditCriteria_Warning.Visible = true;
+                        lbl_EditCriteria_Warning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                    }
+                } // end of using
+            }
+            else
+            {
+                txt_criteriaedit.Focus();
+                panel_EditCriteria_Warning.Visible = true;
+                lbl_EditCriteria_Warning.Text = "You must fill all the requirements";
+            }
+        }
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+        //edit fill text method
+        public void categoryedit_fill(int id)
+        {
+
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("spCriteriaEditFill", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        txt_criteriaedit.Text = rd["criteria_name"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_EditCriteria_Warning.Visible = true;
+                    lbl_EditCriteria_Warning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+            }
+        }
+        //edit fill dropdownmethod
+        public void get_categorydrp()
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Category", con);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    drp_categoryedit.DataSource = rd;
+                    drp_categoryedit.DataBind();
+                    ListItem li = new ListItem("Select Category", "-1");
+                    drp_categoryedit.Items.Insert(0, li);
+
+                }
+                catch (Exception ex)
+                {
+                    txt_criteriaedit.Focus();
+                    panel_EditCriteria_Warning.Visible = true;
+                    lbl_EditCriteria_Warning.Text = "Something went wrong. Try again </br>" + ex.Message;
+                }
+            }
+        }
+    }
+}
