3272da4857b5cf50e6d4b67bff07d32b025976b3
Shahzaib <shahzaibux@gmail.com>
2020-05-05 01:32:14 +0500

    Created Criteria in Admin

---
+++ OnlineAptitudeTest/Admin/Criteria.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="Criteria.aspx.cs" Inherits="OnlineAptitudeTest.Admin.Criteria" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+    <div class="col-md-12">
+        <div class="card">
+            <%--Button For select panel--%>
+            <div class="btn-group bg-warning">
+                <asp:Button ID="btn_panelCriteriaList" runat="server" Text="Criteria List" CssClass="btn btn-info" BorderStyle="None" CausesValidation="False" OnClick="btn_panelCriteriaList_Click" />
+                <asp:Button ID="btn_panelAddCriteria" runat="server" Text="Add Criteria" CssClass="btn btn-info" BorderStyle="None" CausesValidation="False" OnClick="btn_panelAddCriteria_Click" />
+            </div>
+            <%--Add subject panel--%>
+            <asp:Panel ID="panel_AddCriteria" runat="server">
+                <div class="card-body">
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Select Category</label>
+                        <div class="col-md-4">
+                            <asp:DropDownList ID="drp_category" runat="server" CssClass="form-control" DataTextField="category_name" DataValueField="category_id">
+                            </asp:DropDownList>
+                            <asp:RequiredFieldValidator ID="require_drpcategory" runat="server" ErrorMessage="You must select a category" ControlToValidate="drp_category" ForeColor="red" InitialValue="-1"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Criteria Name</label>
+                        <div class="col-md-4">
+                            <asp:TextBox ID="txt_criteria" runat="server" CssClass="form-control"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="require_criteria" runat="server" ErrorMessage="Enter Criteria" ControlToValidate="txt_criteria" ForeColor="red"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="offset-2">
+                            <asp:Button ID="btn_AddCriteria" runat="server" Text="Add Criteria" CssClass="btn" BackColor="#82e0aa" BorderStyle="None" ForeColor="White" OnClick="btn_AddCriteria_Click" />
+                        </div>
+                        <asp:Panel ID="panel_AddCriteria_Warning" runat="server" Visible="false">
+                            <br />
+                            <div class="alert alert-danger text-center">
+                                <asp:Label ID="lbl_AddCriteria_Warning" runat="server" />
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+            </asp:Panel>
+            <%--category list panel--%>
+            <asp:Panel ID="panel_CriteriaList" runat="server">
+                <div class="card text-center mb-3">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <asp:GridView ID="grdview_CriteriaList" runat="server" GridLines="None" AutoGenerateColumns="False" CssClass="table table-bordered" OnRowCommand="grdview_CriteriaList_RowCommand" AllowPaging="True" OnPageIndexChanging="grdview_CriteriaList_PageIndexChanging" PageSize="5">
+                                <Columns>
+                                    <asp:BoundField DataField="criteria_name" HeaderText="Criteria Name" />
+                                    <asp:BoundField DataField="category_name" HeaderText="Category Name" NullDisplayText="The criteria is under no category" />
+                                    <asp:TemplateField HeaderText="Options">
+                                        <ItemTemplate>
+                                            <asp:HyperLink ID="btn_EditCriteria" runat="server" CssClass="btn" BackColor="#3498db" BorderStyle="None" ForeColor="White" NavigateUrl='<%# "~/Admin/EditCriteria.aspx?sid=" + Eval("criteria_id") %>'>
+                                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                                            </asp:HyperLink>
+                                            <asp:LinkButton ID="btn_DeleteCriteria" runat="server" CssClass="btn" BackColor="#e74c3c" BorderStyle="None" ForeColor="White" CommandArgument='<%# Eval("criteria_id") %>' CommandName="DeleteRow">
+                                            <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                            </asp:LinkButton>
+                                        </ItemTemplate>
+                                    </asp:TemplateField>
+                                </Columns>
+                                <EmptyDataTemplate>
+                                    There is no criteria added now. Add criteria
+                                </EmptyDataTemplate>
+                                <PagerStyle CssClass="card-footer" HorizontalAlign="Right" />
+                            </asp:GridView>
+                        </div>
+
+                        <asp:Panel ID="panel_CriteriaList_Warning" runat="server" Visible="false">
+                            <div class="card-footer">
+                                <br />
+                                <div class="alert alert-danger text-center">
+                                    <asp:Label ID="lbl_CriteriaListWarning" runat="server" />
+                                </div>
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+            </asp:Panel>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Admin/Criteria.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest.Admin
+{
+    public partial class Criteria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                panel_CriteriaList.Visible = true;
+                panel_AddCriteria.Visible = false;
+                btn_panelCriteriaList.BackColor = ColorTranslator.FromHtml("#000222");
+                btn_panelAddCriteria.BackColor = ColorTranslator.FromHtml("#ffc107");
+                getCriteriaList();
+
+            }
+        }
+        //This is button for the enable list of subject list panel
+        protected void btn_panelCriteriaList_Click(object sender, EventArgs e)
+        {
+            panel_CriteriaList.Visible = true;
+            panel_AddCriteria.Visible = false;
+            btn_panelCriteriaList.BackColor = ColorTranslator.FromHtml("#000222");
+            btn_panelAddCriteria.BackColor = ColorTranslator.FromHtml("#ffc107");
+            getCriteriaList();
+        }
+        //This is button for the enable list of add subject panel
+        protected void btn_panelAddCriteria_Click(object sender, EventArgs e)
+        {
+            panel_CriteriaList.Visible = false;
+            panel_AddCriteria.Visible = true;
+            btn_panelCriteriaList.BackColor = ColorTranslator.FromHtml("#ffc107");
+            btn_panelAddCriteria.BackColor = ColorTranslator.FromHtml("#000222");
+            get_categorydrp();
+        }
+        //This is button for adding the subject in databse 
+        protected void btn_AddCriteria_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                using (SqlConnection con = new SqlConnection(s))
+                {
+                    SqlCommand cmd = new SqlCommand("insert into Criteria (criteria_name, category_fid) values (@criteria_name,@category_fid)", con);
+                    cmd.Parameters.AddWithValue("@criteria_name", txt_criteria.Text);
+                    cmd.Parameters.AddWithValue("@category_fid", drp_category.SelectedValue);
+                    try
+                    {
+                        con.Open();
+                        int i = cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            Response.Redirect("~/Admin/Criteria.aspx");
+                        }
+                        else
+                        {
+                            txt_criteria.Focus();
+                            panel_AddCriteria_Warning.Visible = true;
+                            lbl_AddCriteria_Warning.Text = "Try again. Criteria is not added";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        txt_criteria.Focus();
+                        panel_AddCriteria_Warning.Visible = true;
+                        lbl_AddCriteria_Warning.Text = "Something went wrong. Criteria is not added </br>" + ex.Message;
+                    }
+                } //end of using
+            }
+            else
+            {
+                txt_criteria.Focus();
+                panel_AddCriteria_Warning.Visible = true;
+                lbl_AddCriteria_Warning.Text = "You must fill all the requirements";
+            }
+
+
+        }
+        //for row command
+        protected void grdview_CriteriaList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteRow")
+            {
+                deleteCriteria(Convert.ToInt32(e.CommandArgument));
+                getCriteriaList();
+            }
+        }
+        //for paging
+        protected void grdview_CriteriaList_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdview_CriteriaList.PageIndex = e.NewPageIndex;
+            getCriteriaList();
+        }
+
+        string s = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+        //method for dropdown
+        public void get_categorydrp()
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Category", con);
+                try
+                {
+                    con.Open();
+                    drp_category.DataSource = cmd.ExecuteReader();
+                    drp_category.DataBind();
+                    ListItem li = new ListItem("Select Category", "-1");
+                    drp_category.Items.Insert(0, li);
+                }
+                catch (Exception ex)
+                {
+                    txt_criteria.Focus();
+                    panel_AddCriteria_Warning.Visible = true;
+                    lbl_AddCriteria_Warning.Text = "Something went wrong. Try again </br>" + ex.Message;
+                }
+            }
+        }
+        // Method for getting subject list
+        public void getCriteriaList()
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("spCriteriaList", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    using (SqlDataAdapter ad = new SqlDataAdapter())
+                    {
+                        ad.SelectCommand = cmd;
+                        using (DataTable dtatble = new DataTable())
+                        {
+                            ad.Fill(dtatble);
+                            grdview_CriteriaList.DataSource = dtatble;
+                            grdview_CriteriaList.DataBind();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_CriteriaList_Warning.Visible = true;
+                    lbl_CriteriaListWarning.Text = "Something went wrong </br>" + ex.Message;
+                }
+            }
+        }
+
+        //Method for delete
+        public void deleteCriteria(int id)
+        {
+            using (SqlConnection con = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("delete Criteria where criteria_id = @criteriaid", con);
+                cmd.Parameters.AddWithValue("@criteriaid", id);
+                try
+                {
+                    con.Open();
+                    int i = (int)cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        Response.Redirect("~/Admin/Criteria.aspx");
+                        Response.Write("Delete Succesfully");
+                    }
+                    else
+                    {
+                        panel_CriteriaList_Warning.Visible = true;
+                        lbl_CriteriaListWarning.Text = "Something went wrong. Can't delete now";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_CriteriaList_Warning.Visible = true;
+                    lbl_CriteriaListWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+
+            }
+        }
+    }
+}
