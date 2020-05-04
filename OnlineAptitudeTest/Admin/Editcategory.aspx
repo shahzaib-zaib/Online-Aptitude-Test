2ab6268e819bb18c3a5116e802c56381a644d07f
Shahzaib <shahzaibux@gmail.com>
2020-05-05 01:41:47 +0500

    Created Editcategory in Admin

---
+++ OnlineAptitudeTest/Admin/Editcategory.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="Editcategory.aspx.cs" Inherits="OnlineAptitudeTest.Admin.Editcategory" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headplaceholder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+    <div class="col-md-12">
+        <div class="card">
+            <%--Button For select panel--%>
+            <div class="btn-group bg-warning">
+                <asp:Button ID="btn_panelCategoryList" runat="server" Text="Edit Category" CssClass="btn btn-info" BorderStyle="None" CausesValidation="False" BackColor="#000222" />
+            </div>
+            <%--Add category --%>
+                <div class="card-body">
+                    <div class="row form-group">
+                        <label class="col-md-2 col-form-label ">Category Name</label>
+                        <div class="col-md-4">
+                            <asp:TextBox ID="txt_categoryedit" runat="server" CssClass="form-control"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="require_category" runat="server" ErrorMessage="Enter category" ControlToValidate="txt_categoryedit" ForeColor="red"></asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="offset-2">
+                            <asp:Button ID="btn_EditCategory" runat="server" Text="Edit Category" CssClass="btn" BackColor="#3498db" BorderStyle="None" ForeColor="White" OnClick="btn_EditCategory_Click"/>
+                        </div>
+                        <asp:Panel ID="panel_EditCategory_Warning" runat="server" Visible="false">
+                            <br />
+                            <div class="alert alert-danger text-center">
+                                <asp:Label ID="lbl_CategoryEditWarning" runat="server" />
+                            </div>
+                        </asp:Panel>
+                    </div>
+                </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ OnlineAptitudeTest/Admin/Editcategory.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAptitudeTest.Admin
+{
+    public partial class Editcategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string category_id = Request.QueryString["cid"];
+                if (category_id == null)
+                {
+                    Response.Redirect("~/Admin/Category.aspx");
+                }
+                txt_categoryedit.Focus();
+                categoryedit_fill(Convert.ToInt32(category_id)); //calling method with parametres
+
+            }
+
+        }
+
+        //for update the category
+        protected void btn_EditCategory_Click(object sender, EventArgs e)
+        {
+            string category_id = Request.QueryString["cid"];
+            if (IsValid)
+            {
+                string cs = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("update Category set category_name= @category_name where category_id = @categoryid", con);
+                    cmd.Parameters.AddWithValue("@categoryid", Convert.ToInt32(category_id));
+                    cmd.Parameters.AddWithValue("@category_name", txt_categoryedit.Text);
+                    try
+                    {
+                        con.Open();
+                        int i = (int)cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            Response.Redirect("~/Admin/Category.aspx");
+                        }
+                        else
+                        {
+                            txt_categoryedit.Focus();
+                            panel_EditCategory_Warning.Visible = true;
+                            lbl_CategoryEditWarning.Text = "Something went wrong. Can't update. Please try after sometime later</br> ";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        txt_categoryedit.Focus();
+                        panel_EditCategory_Warning.Visible = true;
+                        lbl_CategoryEditWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                    }
+                } // end of using 
+            }
+            else
+            {
+                txt_categoryedit.Focus();
+                panel_EditCategory_Warning.Visible = true;
+                lbl_CategoryEditWarning.Text = "You must fill all the requirements";
+            }
+
+        }
+        //edit fill method
+        public void categoryedit_fill(int id)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Category where category_id = @cid", con);
+                cmd.Parameters.AddWithValue("@cid", id);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        txt_categoryedit.Text = rd["category_name"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    panel_EditCategory_Warning.Visible = true;
+                    lbl_CategoryEditWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
+                }
+            }
+        }
+    }
+}
